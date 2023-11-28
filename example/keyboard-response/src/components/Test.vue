7861073b5f14819deeb58b16d8e43c70d1c60bde
lvheng <1834805770@qq.com>
2023-11-28 17:15:35 +0800

    chroe: keyboard-response example add block color

--- example/keyboard-response/src/components/Test.vue
+++ example/keyboard-response/src/components/Test.vue
@@ -1,11 +1,17 @@
 <template>
   <PsychPane name="test" v-slot="data">
-    <div>
-      {{ data.color }}
-    </div>
+    <div class="block" :style="{ backgroundColor: data.color }"></div>
   </PsychPane>
 </template>
 
 <script setup lang="ts">
 import { PsychPane } from '../../../vue-psych'
 </script>
+
+<style scoped>
+.block {
+  width: 280px;
+  height: 280px;
+  border-radius: 50%;
+}
+</style>
